9fb008aac9f8744133ec09dee18e99474e4de44d
Rasyadi Abdoellah <rasyadi.fadilah@btpn.com>
2020-01-27 17:43:00 +0700

    add exclusive filtering behaviour

--- index.js
+++ index.js
@@ -11,10 +11,13 @@ class Filter {
     this._btnClass = 'filter-btn'
     this._eventType = 'click'
     this._isAdditive = true
+    this._isExclusive = false
     //this is binded because below methods are called in an event handler.
     this.filter = this.filter.bind(this)
-    this.resetAll = this.resetAll.bind(this)
-    this.resetOne = this.resetOne.bind(this)
+    this.resetAllUi = this.resetAllUi.bind(this)
+    this.resetFilter = this.resetFilter.bind(this)
+    this.resetFilterGrpUi = this.resetFilterGrpUi.bind(this)
+
   }
 
   static isEmptyObj(obj) {
@@ -82,6 +85,11 @@ class Filter {
     this._isAdditive = false
   }
 
+  setExclusive() {
+    this._isAdditive = false
+    this._isExclusive = true
+  }
+
   filterByType(type, resultObj) {
     // takes a string denoting filter type, and a result obj to store results in
     if (!Filter.isEmptyObj(resultObj)) {
@@ -105,39 +113,49 @@ class Filter {
     //handler that is called on filter event
     const result = {}
     const el = event.target
+    const parent = el.parentNode
     const { filter, value, reset = false } = el.dataset
     let hasFilters = false
 
     //TODO - see if conditional logic can be tidied up
     //TODO - consider renaming resetFilter to something that contextually makes better sense
     if (!reset) { //if not a reset button
+      if (this._isExclusive || (parent.dataset && parent.dataset.filterType === 'exclusive')) {
+        this.resetFilterGrpUi(el)
+        this.resetFilter(el)
+      }
+
       el.classList.toggle(this.toggler) //toggle active class
 
       //check for active class on current element after .toggle() is called. if active, adds element data to filters object. if false, removes data 
       if (el.classList.contains(this.toggler)) {
+        console.log('active')
         this.filters[filter][value] = true //add value to filter obj
         document.querySelector(`[data-filter=${filter}][data-reset="filter"]`).classList.remove(`${this.toggler}`)//remove active from reset button
 
         //current behaviour assumes that all filters on = resetting the filter to show all. This also assumes that if there is a local reset button it is used as the show all button
-        if (this._isAdditive && Filter.isEqualObj(this.filters[filter], this.__filterMap[filter])) {
-          this.resetOne(el)
+        if ((this._isAdditive || (parent.dataset && parent.dataset.filterType === 'additive')) && Filter.isEqualObj(this.filters[filter], this.__filterMap[filter])) {
+          this.resetFilter(el)
+          this.resetFilterGrpUi(el)
           // set the reset button (used as a show all) to active
           document.querySelector(`[data-filter=${filter}][data-reset="filter"]`).classList.add(`${this.toggler}`)
         }
+
+        console.log(this)
       } else {
         delete this.filters[filter][value]
       }
     } else {
       if (reset === 'all') {
-
         //if reset is a reset all, revert to starting button state
-        this.resetAll()
+        this.resetAllUi()
         this.__startingActive.forEach(item => item.classList.add(`${this.toggler}`))
       }
       else if (reset === 'filter') {
         //again, this assumes the filter reset is a show all button
-        this.resetOne(el)
-        if (this._AdditiveFilter) el.classList.add(`${this.toggler}`);
+        this.resetFilter(el)
+        this.resetFilterGrpUi(el)
+        if (this._isAdditive) el.classList.add(`${this.toggler}`);
       }
     }
 
@@ -170,20 +188,24 @@ class Filter {
     }
   }
 
-  resetAll(e) {
+  resetAllUi() {
     document.querySelectorAll(`.${this.selector}.${this.toggler}`).forEach(el => el.classList.remove(this.toggler))
     this._filters = JSON.parse(JSON.stringify(this.initialState))
   }
 
-  resetOne(el) {
-    if (!el.dataset.filter) return Error('filter attribute must be set');
-
-    this._filters[el.dataset.filter] = {}
+  resetFilterGrpUi(el) {
     el = document.querySelector(`[data-filter="${el.dataset.filter}"]`).parentNode.firstElementChild
     do {
       el.classList.remove(`${this.toggler}`)
     } while (el = el.nextElementSibling)
   }
+
+  resetFilter(el) {
+    if (!el.dataset.filter) return Error('filter attribute must be set');
+    this._filters[el.dataset.filter] = {}
+  }
+
+
 
   init() {
     // create a local list of items to filter
